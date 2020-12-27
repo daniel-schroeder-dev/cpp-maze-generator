3ca1596a66c4cbe453a1fc446becdefad42ba617
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-26 18:25:05 -0600

    Create Cell enum and use it as the basis for maze.

--- maze_generator.cpp
+++ maze_generator.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
-char **build_maze(int num_rows, int num_cols);
+enum Cell {
+    C_EMPTY,
+    C_PATH,
+    C_BLOCKED
+};
+
+Cell **build_maze(int num_rows, int num_cols);
 
 int main() {
     int num_rows = 5;
     int num_cols = 5;
-    char **pptr_maze = build_maze(num_rows, num_cols);
+    Cell **pptr_maze = build_maze(num_rows, num_cols);
 
     std::cout << std::endl;
     return 0;
 }
 
-char **build_maze(int num_rows, int num_cols) {
-    char **pptr_maze = new char*[num_rows];
+Cell **build_maze(int num_rows, int num_cols) {
+    Cell **pptr_maze = new Cell*[num_rows];
     for (int row = 0; row < num_rows; row++) {
-        pptr_maze[row] = new char[num_cols];
+        pptr_maze[row] = new Cell[num_cols];
     }
     return pptr_maze;
 }
